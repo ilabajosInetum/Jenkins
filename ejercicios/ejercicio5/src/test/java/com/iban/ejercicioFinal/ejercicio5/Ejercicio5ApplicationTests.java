57a36a37b7303a40eb20bb2d04c0f323a743d931
ilabajosInetum <135244058+ilabajosInetum@users.noreply.github.com>
2023-06-07 13:05:29 +0200

    Add files via upload

---
+++ ejercicios/ejercicio5/src/test/java/com/iban/ejercicioFinal/ejercicio5/Ejercicio5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iban.ejercicioFinal.ejercicio5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
