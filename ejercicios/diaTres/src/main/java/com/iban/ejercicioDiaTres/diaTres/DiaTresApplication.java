c89c4e354190d7d0686d9f158d13f81f6ceb1247
ilabajosInetum <135244058+ilabajosInetum@users.noreply.github.com>
2023-06-07 10:15:06 +0200

    Add files via upload

---
+++ ejercicios/diaTres/src/main/java/com/iban/ejercicioDiaTres/diaTres/DiaTresApplication.java
@@ -0,0 +1,13 @@
+package com.iban.ejercicioDiaTres.diaTres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaTresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaTresApplication.class, args);
+	}
+
+}
---
+++ ejercicios/diaTres/src/test/java/com/iban/ejercicioDiaTres/diaTres/DiaTresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iban.ejercicioDiaTres.diaTres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaTresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
